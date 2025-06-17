cb4a50891784a8b6e1915c4f80090526b92478dc
hsuloong <hsuloong@outlook.om>
2025-06-17 23:13:11 +0800

    add new source

--- app/v2ray/icc.sub/route.ts
+++ app/v2ray/icc.sub/route.ts
@@ -18,22 +18,36 @@ export async function GET() {
                 'https://fgithub.xyz/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
                 'https://fan.194529.xyz/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
                 'https://dgithub.xyz/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
-            ]
-        ]
+            ],
+        ];
 
-    const allPromises: Promise<Response>[] = [];
+    const subUrls = [
+        "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2"
+    ];
+
+    const maxTimeout = 3000;
+
+    const allPublicPromises: Promise<Response>[] = [];
     for (const publicUrlGroup of publicUrlGroups) {
         if (publicUrlGroup.length <= 0) {
             continue;
         }
         const randomIndex = Math.floor(Math.random() * publicUrlGroup.length);
-        allPromises.push(fetch(publicUrlGroup[randomIndex], { signal: AbortSignal.timeout(3000) }));
+        allPublicPromises.push(fetch(publicUrlGroup[randomIndex], { signal: AbortSignal.timeout(maxTimeout) }));
+    }
+
+    const allSubPromises: Promise<Response>[] = [];
+    for (const subUrl of subUrls) {
+        if (subUrls.length <= 0) {
+            continue;
+        }
+        allSubPromises.push(fetch(subUrl, { signal: AbortSignal.timeout(maxTimeout) }));
     }
 
     const re = /("|n|>)((vmess|vless|ssr|ss):\/\/[^"\\<]+)/g;
     let allGWFEscapeUrls: string[] = [];
 
-    for (const onePromise of allPromises) {
+    for (const onePromise of allPublicPromises) {
         try {
             const htmlResponse = await onePromise;
             if (!htmlResponse.ok) {
@@ -52,6 +66,21 @@ export async function GET() {
             console.error('Error fetching URL:', error);
         }
     }
+
+    for (const onePromise of allSubPromises) {
+        try {
+            const htmlResponse = await onePromise;
+            if (!htmlResponse.ok) {
+                continue;
+            }
+
+            const htmlText = Buffer.from((await htmlResponse.text()), 'base64').toString('utf-8');
+            allGWFEscapeUrls.push(...htmlText.split('\n'));
+        } catch (error) {
+            console.error('Error fetching URL:', error);
+        }
+    }
+
     allGWFEscapeUrls = Array.from(new Set(allGWFEscapeUrls));
 
     const responseText = Buffer.from(allGWFEscapeUrls.join("\n"), 'utf-8').toString('base64');
